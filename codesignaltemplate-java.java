76153cb9d4ff67bf0e217a9d7a2cb71e4400d02b
Carlos Cuenca <cuenca.cl@gmail.com>
2020-10-05 21:07:28 -0700

    update template

--- codesignaltemplate-java.java
+++ codesignaltemplate-java.java
@@ -3,16 +3,44 @@
 /// Author: Carlos L. Cuenca
 /// Date: 10/05/20
 
-public class Program {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
+/// --------------
+/// Driver Program
 
+public class Add {
 
+	public static void main(String[] args) throws IOException {
+
+		/// -----------------
+		/// Program Variables
+
+		int inputCount;
+
+		/// -------------
+		/// Program Start
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		inputCount = Integer.parseInt(reader.readLine());
+
+		while(inputCount != 0) {
+
+			// This is a multi-line input
+			String line = reader.readLine();
+			String[] numbers = line.trim().split("\\s+");
+
+			inputCount--;
+
+		}
 
 	}
 
+	/// ------------------------
+	/// Function Implementations
+
 }
 
-/// ------------------------
-/// Function Implementations
 
